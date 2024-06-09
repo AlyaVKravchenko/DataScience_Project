09b5a586be48aeaccd48ea9488161a095a35be09
dovgo <dovgopolyks@gmail.com>
2024-06-09 06:28:46 +0300

    fastapi

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
-# This is a sample Python script.
+from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.templating import Jinja2Templates
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.routers import auth, users, photos
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from src.dependencies.database import get_db
+from src.conf.config import config
+
+app = FastAPI()
+
+app.include_router(auth.router_auth, prefix="/api")
+app.include_router(users.router_users, prefix="/api")
+app.include_router(photos.router_photos, prefix="/api")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+templates = Jinja2Templates(directory=config.BASE_DIR / "src" / "templates")
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        context={"request": request}
+    )
+    # return {"message": "Main page: Python WEB group #1 project"}
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get("/api/healthchecker")
+async def healthchecker(db: AsyncSession = Depends(get_db)):
+    """
+    The healthchecker function is a simple function that checks if the database is up and running.
+    It does this by making a request to the database, and checking if it returns any results.
+    If it doesn't return any results, then we know something's wrong with our connection.
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    :param db: AsyncSession: Pass the database connection to the function
+    :return: A dictionary with the key &quot;message&quot; and value &quot;welcome to fastapi!&quot;
+    """
+    try:
+        # Make request
+        result = await db.execute(text("SELECT 1"))
+        result = result.fetchone()
+        if result is None:
+            raise HTTPException(
+                status_code=500, detail="Database is not configured correctly"
+            )
+        return {"message": "Welcome to FastAPI!"}
+    except Exception as e:
+        print(e)
+        return JSONResponse(status_code=500, content={"detail": "Error connecting to the database"})
+
